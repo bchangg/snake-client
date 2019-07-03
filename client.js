1c0cf1045d9e320312539a65ceb09718bae78e73
Brian Chang <changbhc@gmail.com>
2019-07-03 21:47:55 +0000

    Experimented with moves by sending the server moves and testing out how the server handles instant sequential commands

--- client.js
+++ client.js
@@ -11,17 +11,20 @@ const connect = function() {
   });
   // set encoding for this client to 'utf8'
   conn.setEncoding('utf8');
-  
+
   conn.on('connect', () => {
     console.log("Successfully connected to game server!");
-    // server.write("Name: BHC");
     conn.write("Name: BHC");
+    // setInterval(() => {
+    // conn.write("Move: up"); 
+    // conn.write("Move: right"); 
+    // }, 200);
   });
-  
+
   conn.on('data', (data) => {
     console.log(data);
   });
-  
+
   return conn;
 }
 
