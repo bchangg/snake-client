2bba71a0456863603d32f9e2b88c9bd29c41f776
Brian Chang <changbhc@gmail.com>
2019-07-03 22:09:14 +0000

    Modified play.js to take user input and exit when Ctrl+C

--- play.js
+++ play.js
@@ -1,4 +1,26 @@
-const {connect} = require('./client');
+const {
+  connect
+} = require('./client');
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function(cb) {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', cb);
+  return stdin;
+}
+
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    process.exit();
+  }
+}
 
 console.log('Connecting...');
+setupInput(handleUserInput);
 connect();
